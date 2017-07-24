c7d9f2e45fccae43ddca64570bb02446539289b9
Jilvin <jilvinjacob@gmail.com>
2017-07-25 01:45:19 +0530

    added more functionality

--- js/henosis.js
+++ js/henosis.js
@@ -1,3 +1,11 @@
 $( function() {
-  $( "#henosis" ).draggable();
+  $('#henosis').draggable()
+  .bind('mousedown', function(event, ui){
+    // bring target to front
+    $(event.target.parentElement).append( event.target );
+  }).bind('drag', function(event, ui){
+    // update coordinates manually, since top/left style props don't work on SVG
+    event.target.setAttribute('cx', ui.position.left+30);
+    event.target.setAttribute('cy', ui.position.top+30);
+  });
 } );
