2454fe0af7c3eb6425febc3fc94ec23441a3edad
Jilvin <jilvinjacob@gmail.com>
2017-07-25 11:18:49 +0530

    added henosis containment

--- js/henosis.js
+++ js/henosis.js
@@ -1,11 +1,25 @@
 $( function() {
-  $('#henosis').draggable()
+  $('#henosis').draggable({
+  containment: "window"
+})
   .bind('mousedown', function(event, ui){
     // bring target to front
     $(event.target.parentElement).append( event.target );
   }).bind('drag', function(event, ui){
-    // update coordinates manually, since top/left style props don't work on SVG
-    event.target.setAttribute('cx', ui.position.left+30);
-    event.target.setAttribute('cy', ui.position.top+30);
+    // var xMin = 0;
+    // var xMax =300;
+    // var yMin = 0;
+    // var yMax = 300;
+    var leftPosition = ui.position.left+30;
+    var topPosition = ui.position.top+30;
+    // if(leftPosition>=xMin && leftPosition<=xMax)
+    // {
+      // update coordinates manually, since top/left style props don't work on SVG
+      event.target.setAttribute('cx', leftPosition);
+    // }
+    // if(topPosition>=yMin && topPosition<=yMax)
+    // {
+      event.target.setAttribute('cy', topPosition);
+    // }
   });
 } );
