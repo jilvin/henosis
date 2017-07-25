afa5e0931e21aed8c46b333b6138068e0f603ebb
Jilvin <jilvinjacob@gmail.com>
2017-07-25 14:42:20 +0530

    bugfix containment

--- js/henosis.js
+++ js/henosis.js
@@ -1,13 +1,13 @@
 $( function() {
+  var windowHeight = $(window).height();
+  var windowWidth = $(window).width();
   $('#henosis').draggable({
-    containment: "window"
+    containment: [ 0, 0, windowWidth-40, windowHeight-40]
   })
   .bind('mousedown', function(event, ui){
     // bring target to front
     $(event.target.parentElement).append( event.target );
   }).bind('drag', function(event, ui){
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
     var leftPosition = ui.position.left+(windowWidth/2);
     var topPosition = ui.position.top+(windowHeight/2);
     // update coordinates manually, since top/left style props don't work on SVG
